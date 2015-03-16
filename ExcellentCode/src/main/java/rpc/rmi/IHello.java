1b422f9fb88ab5145107e5419b941afa202e49b5
Galen <zhbdlgjc@163.com>
2015-03-16 17:10:45 +0800

    RMI

---
+++ ExcellentCode/src/main/java/rpc/rmi/IHello.java
@@ -0,0 +1,22 @@
+package rpc.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IHello extends Remote {
+
+    /**
+     * 绠�崟鐨勮繑鍥炩�Hello World锛�瀛楁牱
+     * @return 杩斿洖鈥淗ello World锛�瀛楁牱
+     * @throws java.rmi.RemoteException
+     */
+    public String helloWorld() throws RemoteException;
+
+    /**
+     * 涓�釜绠�崟鐨勪笟鍔℃柟娉曪紝鏍规嵁浼犲叆鐨勪汉鍚嶈繑鍥炵浉搴旂殑闂�璇�
+     * @param someBodyName  浜哄悕
+     * @return 杩斿洖鐩稿簲鐨勯棶鍊欒
+     * @throws java.rmi.RemoteException
+     */
+    public String sayHelloToSomeBody(String someBodyName) throws RemoteException;
+}
---
+++ ExcellentCode/src/main/java/rpc/rmi/RMIClient.java
@@ -0,0 +1,45 @@
+package rpc.rmi;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class RMIClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+            //在RMI服务注册表中查找名称为RHello的对象，并调用其上的方法
+			IHello rhello =(IHello) Naming.lookup("//localhost:9813/RHello");
+            rhello.sayHelloToSomeBody("lisi");
+/*
+            URL[] urls = new URL[] {new URL("file:/" + "d:/src/")}; 
+            URLClassLoader ul = new URLClassLoader(urls); 
+            Class c = ul.loadClass("com.bjsxt.proxy.$Proxy1"); 
+            System.out.println(c); 
+            Constructor ctr = c.getConstructor(InvocationHandler.class); 
+            Object m = ctr.newInstance(h); */
+            
+            
+            
+            System.out.println(rhello.helloWorld());
+//            System.out.println(rhello.sayHelloToSomeBody("熔岩"));
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();  
+        } 
+
+	}
+
+}
